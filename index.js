0e76bcc2df45f2241f33fb4163abe5a66fcd1913
k0robova <alonakorobova@ukr.net>
2024-01-15 16:09:24 +0200

    done

---
+++ index.js
@@ -0,0 +1,43 @@
+const { program } = require("commander");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await listContacts();
+      return console.table(allContacts);
+
+    case "get":
+      const contact = await getContactById(id);
+      return console.log(contact);
+
+    case "add":
+      const newContact = await addContact(name, email, phone);
+      return console.log(newContact);
+
+    case "remove":
+      const contactDelete = await removeContact(id);
+      return console.log(contactDelete);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
